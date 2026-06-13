1887776da8e59ff3ec0005a8f2447947b9d2cf28
Chinmay Pandey <chinmayworkpandey@gmail.com>
2026-06-13 17:41:38 +0530

    Updates

--- _next/static/6ehMMeS5sBr5L8xfBye4-/_buildManifest.js
+++ _next/static/6ehMMeS5sBr5L8xfBye4-/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/PrepSync.ai//_next/:path+",
+        "destination": "/PrepSync.ai/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
